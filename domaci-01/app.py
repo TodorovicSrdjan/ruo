abf18829b93a081c45caac15b9998e2027dbd99a
TodorovicSrdjan <tsrdjan@protonmail.com>
2024-03-24 19:16:05 +0100

    Dodaj src za d01

---
+++ domaci-01/app.py
@@ -0,0 +1,32 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "Test"
+
+@app.post("/todo")
+def create_todo():
+    return "create todo item"
+
+@app.get("/todo/{id}")
+def read_todo(id: int):
+    return "read todo item with id {id}"
+
+@app.put("/todo/{id}")
+def update_todo(id: int):
+    return "update todo item with id {id}"
+
+@app.delete("/todo/{id}")
+def delete_todo(id: int):
+    return "delete todo item with id {id}"
+
+@app.get("/todo")
+def read_todo_list():
+    return "read todo list"
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", log_level="info")
